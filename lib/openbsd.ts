c8561ada2c4adcbf8cbc0bcee0882de8041dfdc2
Eric Lenio <eric@lincware.com>
2023-04-12 10:13:33 -0700

    add

---
+++ lib/openbsd.ts
@@ -0,0 +1,58 @@
+import fs from 'fs';
+import path from 'path';
+import { log } from './log';
+import { spawnSync, SpawnSyncOptions } from 'child_process';
+
+const NODE_PORTS_DIR="/usr/ports/lang/node";
+const IS_OPENBSD=process.platform==='openbsd';
+if (IS_OPENBSD && !fs.existsSync(NODE_PORTS_DIR)) {
+  throw `expected ${NODE_PORTS_DIR} to exist, see: https://www.openbsd.org/faq/ports/ports.html`;
+}
+
+const getMakeVariable=(VARNAME: string)=>spawnSync("make",[`show=${VARNAME}`],{cwd:NODE_PORTS_DIR}).stdout.toString().trim();
+
+process.env.MAKE_JOB_COUNT ??= 1;
+// https://man.openbsd.org/bsd.port.mk.5#WRKOBJDIR
+process.env.WRKOBJDIR ??= `/tmp/pkg-fetch`;
+// https://man.openbsd.org/bsd.port.mk.5#PKGNAME
+const PKGNAME = getMakeVariable('PKGNAME');
+// https://man.openbsd.org/bsd.port.mk.5#DISTNAME
+const DISTNAME = getMakeVariable('DISTNAME');
+// https://man.openbsd.org/bsd.port.mk.5#WRKSRC
+const WRKSRC = getMakeVariable('WRKSRC');
+//process.env.PKG_BUILD_PATH ??= `${process.env.WRKOBJDIR}/${PKGNAME}/${DISTNAME}`;
+process.env.PKG_BUILD_PATH ??= `${process.env.WRKOBJDIR}/${PKGNAME}`;
+log.info(`PKG_BUILD_PATH: ${process.env.PKG_BUILD_PATH}`);
+
+const mySpawn=(cmd:string,cmdArgs?:string[],options?:SpawnSyncOptions)=>{
+  const result=spawnSync(cmd,cmdArgs,options);
+  if (result.error) {
+    throw result.error;
+  }
+  return result;
+};
+
+const prepare=()=>{
+  if (! IS_OPENBSD) {
+    return;
+  }
+  log.info(`preparing for build on OpenBSD (using ports dir ${NODE_PORTS_DIR})`);
+  mySpawn("make",['clean','extract'],{cwd:NODE_PORTS_DIR,stdio:'inherit'});
+  // undo env.cc patch: need to restore the original way of how execPath is
+  // computed because otherwise process.execPath gets hard-coded to
+  // "/usr/local/bin/node" and that will break pkg's bootstrapping logic
+  mySpawn("mv",[`${WRKSRC}/src/env.cc.orig.port`,`${WRKSRC}/src/env.cc`],{stdio:'inherit'});
+  mySpawn("ln",["-s",DISTNAME,"node"],{cwd:process.env.PKG_BUILD_PATH,stdio:'inherit'});
+};
+
+const compile=()=>{
+  log.info('start OpenBSD compile');
+  mySpawn("make",[`-j${process.env.MAKE_JOB_COUNT}`,'build'],{cwd:NODE_PORTS_DIR,stdio:'inherit'});
+  return path.join(WRKSRC,"out","Release","node");
+};
+
+export default {
+  prepare,
+  compile,
+  isOpenbsd:IS_OPENBSD,
+};
